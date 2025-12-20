521ad1621df97c7bd9ba4426734e7a34d4f65174
ThanhLoc86 <loccntthutech@gmail.com>
2025-12-21 00:56:32 +0700

    Push

---
+++ ChessEngine/src/chessengine/ChessEngine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chessengine;
+
+/**
+ *
+ * @author loccn
+ */
+public class ChessEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
